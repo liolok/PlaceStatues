4d9a156690be367263f95f7d930689f45acfaaf3
liolok <i@liolok.com>
2025-03-25 17:26:49 +0800

    refactor alias

--- modmain.lua
+++ modmain.lua
@@ -1,23 +1,11 @@
 modimport('keybind')
 
-PrefabFiles = {
-  'CirclePlacer',
-}
-Assets = {
-  Asset('ANIM', 'anim/circleplacer.zip'),
-}
+PrefabFiles = { 'CirclePlacer' }
+Assets = { Asset('ANIM', 'anim/circleplacer.zip') }
 
-local _G = GLOBAL
+local G = GLOBAL
 
-local require = _G.require
-local SendRPCToServer = _G.SendRPCToServer
-local TheInput = _G.TheInput
-local Vector3 = _G.Vector3
-local ACTIONS = _G.ACTIONS
-local RPC = _G.RPC
-local ThePlayer
-local TheWorld
-local BufferedAction = _G.BufferedAction
+local require = G.require
 
 local Task = nil
 local Timer = 0.8
@@ -40,11 +28,11 @@ local Indicate = false
 local Carrying = false
 
 local function IsWalkButtonDown()
-  return _G.ThePlayer.components.playercontroller:IsAnyOfControlsPressed(
-    _G.CONTROL_MOVE_UP,
-    _G.CONTROL_MOVE_DOWN,
-    _G.CONTROL_MOVE_LEFT,
-    _G.CONTROL_MOVE_RIGHT
+  return G.ThePlayer.components.playercontroller:IsAnyOfControlsPressed(
+    G.CONTROL_MOVE_UP,
+    G.CONTROL_MOVE_DOWN,
+    G.CONTROL_MOVE_LEFT,
+    G.CONTROL_MOVE_RIGHT
   )
 end
 
@@ -60,30 +48,14 @@ function round(num)
   end
 end
 
-local Tempo
-local function HightlightTempo(x, z)
-  if not Indicate and not Carrying then return end
-  if not Tempo then
-    if _G.PrefabExists('circleplacer') then
-      Tempo = _G.SpawnPrefab('circleplacer')
-      Tempo.AnimState:PlayAnimation('off', true)
-      Tempo.Transform:SetScale(1, 1, 1)
-    else
-      Tempo = _G.SpawnPrefab('gridplacer')
-    end
-  end
-  Tempo:Show()
-  Tempo.Transform:SetPosition(x, -0.1, z)
-end
-
 local function NewPoint()
   local NPt
-  if _G.PrefabExists('circleplacer') then
-    NPt = _G.SpawnPrefab('circleplacer')
+  if G.PrefabExists('circleplacer') then
+    NPt = G.SpawnPrefab('circleplacer')
     NPt.AnimState:PlayAnimation('off', true)
     NPt.Transform:SetScale(0.5, 0.5, 0.5)
   else
-    NPt = _G.SpawnPrefab('gridplacer')
+    NPt = G.SpawnPrefab('gridplacer')
   end
   return NPt
 end
@@ -132,7 +104,7 @@ local function TurnCircle(pt)
   local bgx = pt.x
   local bgz = pt.z
 
-  if CenterLocate == nil then return Vector3(bgx, 0, bgz) end
+  if CenterLocate == nil then return G.Vector3(bgx, 0, bgz) end
 
   local x = CenterLocate.x
   local z = CenterLocate.z
@@ -186,13 +158,13 @@ local function TurnCircle(pt)
     bgx = x + Dltx
     bgz = z + Dltz
   end
-  return Vector3(bgx, 0, bgz)
+  return G.Vector3(bgx, 0, bgz)
 end
 
 local function Lineing(pt)
   local bgx = pt.x
   local bgz = pt.z
-  if CenterLocate == nil or SecondLocate == nil then return Vector3(bgx, 0, bgz) end
+  if CenterLocate == nil or SecondLocate == nil then return G.Vector3(bgx, 0, bgz) end
   local x = CenterLocate.x
   local z = CenterLocate.z
   local DeltaX = bgx - x
@@ -201,7 +173,7 @@ local function Lineing(pt)
   local DeltaAngZ = SecondLocate.z - z
 
   local rdiour = math.sqrt(DeltaAngX ^ 2 + DeltaAngZ ^ 2)
-  if rdiour == 0 then return Vector3(bgx, 0, bgz) end
+  if rdiour == 0 then return G.Vector3(bgx, 0, bgz) end
 
   local DireAng = math.atan2(DeltaAngX, DeltaAngZ)
   local radius = math.sqrt(DeltaX ^ 2 + DeltaZ ^ 2)
@@ -221,7 +193,7 @@ local function Lineing(pt)
   bgz = z + Dltz
 
   if Closet then
-    local Direeer = Vector3(DeltaAngX, 0, DeltaAngZ) / rdiour
+    local Direeer = G.Vector3(DeltaAngX, 0, DeltaAngZ) / rdiour
 
     for i = 0, 7 do
       if i < 4 then
@@ -233,26 +205,25 @@ local function Lineing(pt)
       end
     end
   end
-  return Vector3(bgx, 0, bgz)
+  return G.Vector3(bgx, 0, bgz)
 end
 
-local function InGame() return ThePlayer and ThePlayer.HUD and not ThePlayer.HUD:HasInputFocus() end
+local function InGame() return G.ThePlayer and G.ThePlayer.HUD and not G.ThePlayer.HUD:HasInputFocus() end
 
 local function Distance(PointX, PointZ)
-  local xx, yy, zz = ThePlayer.Transform:GetWorldPosition()
+  local xx, yy, zz = G.ThePlayer.Transform:GetWorldPosition()
   local DelX = PointX - xx
   local DelZ = PointZ - zz
   return math.sqrt(DelX * DelX + DelZ * DelZ)
 end
 
 local function LongWalk(pos)
-  local PlayerController = ThePlayer.components.playercontroller
-  local act = BufferedAction(ThePlayer, nil, ACTIONS.WALKTO, ThePlayer.replica.inventory:GetActiveItem(), pos)
-  if PlayerController:CanLocomote() then
-    act.preview_cb = function() SendRPCToServer(RPC.LeftClick, act.action.code, pos.x, pos.z, nil, true) end
-    PlayerController:DoAction(act)
+  local act = G.BufferedAction(G.ThePlayer, nil, G.ACTIONS.WALKTO, G.ThePlayer.replica.inventory:GetActiveItem(), pos)
+  if G.ThePlayer.components.playercontroller:CanLocomote() then
+    act.preview_cb = function() G.SendRPCToServer(G.RPC.LeftClick, act.action.code, pos.x, pos.z, nil, true) end
+    G.ThePlayer.components.playercontroller:DoAction(act)
   else
-    SendRPCToServer(RPC.LeftClick, act.action.code, pos.x, pos.z, nil, nil, nil, act.action.canforce)
+    G.SendRPCToServer(G.RPC.LeftClick, act.action.code, pos.x, pos.z, nil, nil, nil, act.action.canforce)
   end
 end
 
@@ -260,16 +231,16 @@ local function ShortStep(pos)
   if pos then
     local Distate = Distance(pos.x, pos.z)
     if Distate > 0 then
-      local xx, yy, zz = ThePlayer.Transform:GetWorldPosition()
-      local Direct = (Vector3(pos.x - xx, 0, pos.z - zz) / Distate) * 0.165
-      local Destination = Vector3(xx + Direct.x, 0, zz + Direct.z)
+      local xx, yy, zz = G.ThePlayer.Transform:GetWorldPosition()
+      local Direct = (G.Vector3(pos.x - xx, 0, pos.z - zz) / Distate) * 0.165
+      local Destination = G.Vector3(xx + Direct.x, 0, zz + Direct.z)
       LongWalk(Destination)
     end
   end
 end
 
 local function GetRoutePos(Radius0, x1, z1, Radius1)
-  local x0, y0, z0 = ThePlayer.Transform:GetWorldPosition()
+  local x0, y0, z0 = G.ThePlayer.Transform:GetWorldPosition()
   local d = math.sqrt((x1 - x0) * (x1 - x0) + (z1 - z0) * (z1 - z0))
   if d == 0 then return nil end
   local a = (Radius0 * Radius0 - Radius1 * Radius1 + d * d) / (2 * d)
@@ -282,23 +253,23 @@ local function GetRoutePos(Radius0, x1, z1, Radius1)
   local z3 = z2 - h * (x1 - x0) / d
   --print(tostring(math.sqrt((x3-x1)*(x3-x1) + (z3-z1)*(z3-z1))))
 
-  return Vector3(x3, 0, z3)
+  return G.Vector3(x3, 0, z3)
 end
 
 local PreviousPlayerPost = nil
 local AngleStep = true
 
 local function SayFinalDistance(inst, PointX, PointZ)
-  GLOBAL.ThePlayer.components.talker:Say(tostring(Distance(PointX, PointZ)))
+  G.ThePlayer.components.talker:Say(tostring(Distance(PointX, PointZ)))
 end
 
 local function Wonlerina(inst, PointX, PointZ, StepDistance)
   local Distate = 0
   if IsWalkButtonDown() then return end
-  if GLOBAL.ThePlayer:HasTag('idle') and not GLOBAL.ThePlayer.components.playercontroller:IsDoingOrWorking() then
+  if G.ThePlayer:HasTag('idle') and not G.ThePlayer.components.playercontroller:IsDoingOrWorking() then
     if StepDistance > 0 then
       if Distance(PointX, PointZ) > 0.3 then
-        LongWalk(Vector3(PointX, 0, PointZ))
+        LongWalk(G.Vector3(PointX, 0, PointZ))
       else
         if AngleStep then
           local RelativeDistance = math.floor(Distance(PointX, PointZ) / StepDistance)
@@ -310,7 +281,7 @@ local function Wonlerina(inst, PointX, PointZ, StepDistance)
             else
               AngleStep = true
               Task = nil
-              ThePlayer:DoTaskInTime(Timer, SayFinalDistance, PointX, PointZ)
+              G.ThePlayer:DoTaskInTime(Timer, SayFinalDistance, PointX, PointZ)
               return
             end
           else
@@ -320,25 +291,25 @@ local function Wonlerina(inst, PointX, PointZ, StepDistance)
             else
               AngleStep = true
               Task = nil
-              ThePlayer:DoTaskInTime(Timer, SayFinalDistance, PointX, PointZ)
+              G.ThePlayer:DoTaskInTime(Timer, SayFinalDistance, PointX, PointZ)
               return
             end
           end
         else
-          ShortStep(Vector3(PointX, 0, PointZ))
+          ShortStep(G.Vector3(PointX, 0, PointZ))
           AngleStep = true
           Task = nil
-          ThePlayer:DoTaskInTime(Timer, SayFinalDistance, PointX, PointZ)
+          G.ThePlayer:DoTaskInTime(Timer, SayFinalDistance, PointX, PointZ)
           return
         end
       end
     else
       if PreviousPlayerPost == nil then
-        local xx, yy, zz = ThePlayer.Transform:GetWorldPosition()
-        PreviousPlayerPost = Vector3(xx, yy, zz)
-        ShortStep(Vector3(PointX, 0, PointZ))
+        local xx, yy, zz = G.ThePlayer.Transform:GetWorldPosition()
+        PreviousPlayerPost = G.Vector3(xx, yy, zz)
+        ShortStep(G.Vector3(PointX, 0, PointZ))
       else
-        local xx, yy, zz = ThePlayer.Transform:GetWorldPosition()
+        local xx, yy, zz = G.ThePlayer.Transform:GetWorldPosition()
         local xD = PreviousPlayerPost.x - xx
         local zD = PreviousPlayerPost.z - zz
         local Dist = math.sqrt(xD * xD + zD * zD)
@@ -348,9 +319,9 @@ local function Wonlerina(inst, PointX, PointZ, StepDistance)
     end
   end
   if Distate > 0 then
-    Task = ThePlayer:DoTaskInTime(Timer, Wonlerina, PointX, PointZ, Distate)
+    Task = G.ThePlayer:DoTaskInTime(Timer, Wonlerina, PointX, PointZ, Distate)
   else
-    Task = ThePlayer:DoTaskInTime(Timer, Wonlerina, PointX, PointZ, StepDistance)
+    Task = G.ThePlayer:DoTaskInTime(Timer, Wonlerina, PointX, PointZ, StepDistance)
   end
 end
 
@@ -358,8 +329,8 @@ local function GetCorrectPoint()
   for i = 0, 7 do
     Closet[i][1] = false
   end
-  local x = TheInput:GetWorldPosition().x
-  local z = TheInput:GetWorldPosition().z
+  local x = G.TheInput:GetWorldPosition().x
+  local z = G.TheInput:GetWorldPosition().z
 
   if CorrectionSetting == 0 then -- Grid
     if not GridTweak then
@@ -384,13 +355,13 @@ local function GetCorrectPoint()
       end
     end
   elseif CorrectionSetting == 1 then -- Circle
-    local SD = Vector3(x, y, z)
+    local SD = G.Vector3(x, y, z)
     local CircleSet = TurnCircle(SD)
     x = CircleSet.x
     z = CircleSet.z
     --AlreadyShowIndicator
   elseif CorrectionSetting == 2 then -- Hexa
-    if not CenterLocate then return Vector3(x, 0, z) end
+    if not CenterLocate then return G.Vector3(x, 0, z) end
 
     if not GridTweak then
       if round((z - CenterLocate.z) / 0.86602540378) % 2 ~= 1 then
@@ -433,35 +404,35 @@ local function GetCorrectPoint()
       end
     end
   elseif CorrectionSetting == 3 then -- Line
-    local SD = Vector3(x, y, z)
+    local SD = G.Vector3(x, y, z)
     local CircleSet = Lineing(SD)
     x = CircleSet.x
     z = CircleSet.z
   elseif CorrectionSetting == 4 then -- PentaCircle
-    --local SD = Vector3(x,y,z)
+    --local SD = G.Vector3(x,y,z)
     --local CircleSet = TurnPentaCircle(SD) -- a function in my private version of this mod
     --x = CircleSet.x
     --z = CircleSet.z
   elseif CorrectionSetting == 5 then -- CustomPentaLine
-    --local SD = Vector3(x,y,z)
+    --local SD = G.Vector3(x,y,z)
     --local CircleSet = PentaLine(SD)	-- a function in my private version of this mod
     --x = CircleSet.x
     --z = CircleSet.z
   end
 
-  return Vector3(x, 0, z)
+  return G.Vector3(x, 0, z)
 end
 
 local gridplacer
 local function HightlightDrop()
   if not Indicate and not Carrying then return end
   if not gridplacer then
-    if _G.PrefabExists('circleplacer') then
-      gridplacer = _G.SpawnPrefab('circleplacer')
+    if G.PrefabExists('circleplacer') then
+      gridplacer = G.SpawnPrefab('circleplacer')
       gridplacer.AnimState:PlayAnimation('anim', true)
       gridplacer.Transform:SetScale(0.8, 0.8, 0.8)
     else
-      gridplacer = _G.SpawnPrefab('gridplacer')
+      gridplacer = G.SpawnPrefab('gridplacer')
     end
   end
   gridplacer:Show()
@@ -474,12 +445,12 @@ local DropCenter
 local function HightlightCenter()
   if not CenterLocate then return end
   if not DropCenter then
-    if _G.PrefabExists('circleplacer') then
-      DropCenter = _G.SpawnPrefab('circleplacer')
+    if G.PrefabExists('circleplacer') then
+      DropCenter = G.SpawnPrefab('circleplacer')
       DropCenter.AnimState:PlayAnimation('on', true)
       DropCenter.Transform:SetScale(1, 1, 1)
     else
-      DropCenter = _G.SpawnPrefab('gridplacer')
+      DropCenter = G.SpawnPrefab('gridplacer')
     end
   end
   if CorrectionSetting ~= 0 then
@@ -494,12 +465,12 @@ local SecondDrop
 local function HightlightSecond()
   if not SecondLocate then return end
   if not SecondDrop then
-    if _G.PrefabExists('circleplacer') then
-      SecondDrop = _G.SpawnPrefab('circleplacer')
+    if G.PrefabExists('circleplacer') then
+      SecondDrop = G.SpawnPrefab('circleplacer')
       SecondDrop.AnimState:PlayAnimation('off', true)
       SecondDrop.Transform:SetScale(1, 1, 1)
     else
-      SecondDrop = _G.SpawnPrefab('gridplacer')
+      SecondDrop = G.SpawnPrefab('gridplacer')
     end
   end
   if CorrectionSetting == 3 then
@@ -520,44 +491,44 @@ end
 
 local function ChangePlacementType()
   if not CenterLocate then
-    local xx = round(TheInput:GetWorldPosition().x)
-    local zz = round(TheInput:GetWorldPosition().z)
-    CenterLocate = Vector3(xx, 0, zz)
+    local xx = round(G.TheInput:GetWorldPosition().x)
+    local zz = round(G.TheInput:GetWorldPosition().z)
+    CenterLocate = G.Vector3(xx, 0, zz)
   end
   if not SecondLocate then
-    local xx, yy, zz = ThePlayer.Transform:GetWorldPosition()
+    local xx, yy, zz = G.ThePlayer.Transform:GetWorldPosition()
     xx = round(xx)
     zz = round(zz)
-    SecondLocate = Vector3(xx, 0, zz)
+    SecondLocate = G.Vector3(xx, 0, zz)
   end
   HideAll()
   CorrectionSetting = (CorrectionSetting + 1) % TotalSetting
-  GLOBAL.ThePlayer.components.talker:Say(Settings[CorrectionSetting])
+  G.ThePlayer.components.talker:Say(Settings[CorrectionSetting])
 end
 
 local function ToggleAlignTarget()
   GridTweak = not GridTweak
   if GridTweak then
-    GLOBAL.ThePlayer.components.talker:Say('[Place Statues] Align to Wall')
+    G.ThePlayer.components.talker:Say('[Place Statues] Align to Wall')
   else
-    GLOBAL.ThePlayer.components.talker:Say('[Place Statues] Align to Turf')
+    G.ThePlayer.components.talker:Say('[Place Statues] Align to Turf')
   end
 end
 
 local function SetFirstPoint()
   if not InGame() then return end
-  -- local xx = round(TheInput:GetWorldPosition().x)
-  -- local zz = round(TheInput:GetWorldPosition().z)
-  local p = TheInput:GetWorldPosition()
-  CenterLocate = Vector3(round(p.x), 0, round(p.z))
+  -- local xx = round(G.TheInput:GetWorldPosition().x)
+  -- local zz = round(G.TheInput:GetWorldPosition().z)
+  local p = G.TheInput:GetWorldPosition()
+  CenterLocate = G.Vector3(round(p.x), 0, round(p.z))
 end
 
 local function SetSecondPoint()
   if not InGame() then return end
-  -- local xx = round(TheInput:GetWorldPosition().x)
-  -- local zz = round(TheInput:GetWorldPosition().z)
-  local p = TheInput:GetWorldPosition()
-  SecondLocate = Vector3(round(p.x), 0, round(p.z))
+  -- local xx = round(G.TheInput:GetWorldPosition().x)
+  -- local zz = round(G.TheInput:GetWorldPosition().z)
+  local p = G.TheInput:GetWorldPosition()
+  SecondLocate = G.Vector3(round(p.x), 0, round(p.z))
   HightlightSecond()
 end
 
@@ -579,9 +550,9 @@ local function TogglePreciseWalk()
   --Turn on/off indicator
   Indicate = not Indicate
   if not Indicate then
-    GLOBAL.ThePlayer.components.talker:Say('[Place Statues] Precise Walk Disabled')
+    G.ThePlayer.components.talker:Say('[Place Statues] Precise Walk Disabled')
   else
-    GLOBAL.ThePlayer.components.talker:Say(
+    G.ThePlayer.components.talker:Say(
       '[Place Statues] Precision Walk Enabled\nPlease disable lag compensation\n Use  to walk'
     )
   end
@@ -601,11 +572,11 @@ function KeyBind(name, key)
   if handler[name] then handler[name]:Remove() end -- disable old binding
   if key ~= nil then -- new binding
     if key >= 1000 then -- it's a mouse button
-      handler[name] = GLOBAL.TheInput:AddMouseButtonHandler(function(button, down, x, y)
+      handler[name] = G.TheInput:AddMouseButtonHandler(function(button, down, x, y)
         if button == key and down then callback[name]() end
       end)
     else -- it's a keyboard key
-      handler[name] = GLOBAL.TheInput:AddKeyDownHandler(key, callback[name])
+      handler[name] = G.TheInput:AddKeyDownHandler(key, callback[name])
     end
   else -- no binding
     handler[name] = nil
@@ -613,9 +584,7 @@ function KeyBind(name, key)
 end
 
 AddComponentPostInit('playercontroller', function(self)
-  ThePlayer = _G.ThePlayer
-  TheWorld = _G.TheWorld
-  ThePlayer:DoTaskInTime(0, ClosetGenerate)
+  G.ThePlayer:DoTaskInTime(0, ClosetGenerate)
 end)
 
 PlayerController = require('components/playercontroller')
@@ -623,8 +592,8 @@ local OldOnUpdate = PlayerController.OnUpdate
 
 function PlayerController:OnUpdate(Time)
   if InGame() then
-    local StatueB = _G.EQUIPSLOTS.BODY
-    local Statue = ThePlayer.replica.inventory:GetEquippedItem(StatueB)
+    local StatueB = G.EQUIPSLOTS.BODY
+    local Statue = G.ThePlayer.replica.inventory:GetEquippedItem(StatueB)
     if Statue and Statue:HasTag('heavy') and GetModConfigData('auto_precise_walk') then
       Carrying = true
     else
@@ -638,14 +607,14 @@ end
 local OldOnLeftClick = PlayerController.OnLeftUp
 
 function PlayerController:OnLeftUp()
-  if InGame() and IsIndicate and TheInput:GetHUDEntityUnderMouse() == nil then
+  if InGame() and IsIndicate and G.TheInput:GetHUDEntityUnderMouse() == nil then
     if Task ~= nil then
       Task:Cancel()
       Task = nil
     end
 
     local Posit = GetCorrectPoint()
-    Task = ThePlayer:DoTaskInTime(Timer, Wonlerina, Posit.x, Posit.z, 0)
+    Task = G.ThePlayer:DoTaskInTime(Timer, Wonlerina, Posit.x, Posit.z, 0)
   end
   return OldOnLeftClick(self)
 end
